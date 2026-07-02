b9c2313ff77eaba865ecd36dd6670011efcd2068
alhadwan <ali.hadwan@ymail.com>
2026-07-01 23:43:18 -0700

    feat: implement integration test for add to cart button in the product page

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -5,6 +5,12 @@ import { Product } from "./Product";
 // render -> display a component in a fake web page,
 // screen -> check if the component is displayed correctly
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import axios from "axios";
+
+// mocking the axios package to get a fake version of axios version.
+// this way, the backend data dose not get mutated during tests.
+vi.mock("axios");
 
 describe("product component", () => {
   it("displays the elements in the components", () => {
@@ -21,7 +27,7 @@ describe("product component", () => {
       keywords: ["socks", "sports", "apparel"],
     };
 
-    // using vi.fn to create an empty function so it dose not contact
+    // using vi.fn to create an empty function(mock) so it dose not contact
     // the backend and mutate the data.
     const loadCart = vi.fn();
 
@@ -41,5 +47,36 @@ describe("product component", () => {
       "images/ratings/rating-45.png",
     );
     expect(screen.getByText("87")).toBeInTheDocument();
+  });
+
+  it("adds products to the cart", async () => {
+    const product = {
+      id: "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
+      image:
+        "http://localhost:5173/images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87,
+      },
+      priceCents: 1099,
+      keywords: ["socks", "sports", "apparel"],
+    };
+
+    // using vi.fn to create an empty function(mock) so it dose not contact
+    // the backend and mutate the data.
+    const loadCart = vi.fn();
+
+    render(<Product product={product} loadCart={loadCart} />);
+
+    const user = userEvent.setup();
+    const addToCart = screen.getByTestId("add-to-cart-button-test");
+    await user.click(addToCart);
+
+    expect(axios.post).toHaveBeenCalledWith("api/cart-items", {
+      productId: product.id,
+      quantity: 1,
+    });
+    expect(loadCart).toHaveBeenCalled();
   });
 });
